002546707ee13e821f50f6ce096ffd932485b70f
Dave <davidecannistraro02@gmail.com>
2026-02-17 22:37:02 +0100

    s7 lib

--- backend/routes/variables.js
+++ backend/routes/variables.js
@@ -1,38 +1,38 @@
-const pool = require('../db');
+const pool = require("../db");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-
-    try {
-        const dbRes = await pool.query('SELECT * FROM config;');
-        res.json(dbRes.rows);
-    } catch (e) {
-        res.status(404).json({
-            message: 'Error fetching variables'
-        });
-    };
-
+router.get("/", async (req, res) => {
+  try {
+    const dbRes = await pool.query("SELECT * FROM config;");
+    res.json(dbRes.rows);
+  } catch (e) {
+    res.status(404).json({
+      message: "Error fetching variables",
+    });
+  }
 });
 
-router.post('/', async (req, res) => {
-    const { body } = req;
-    const { name, address, data_type, sampling_interval } = body;
+router.post("/", async (req, res) => {
+  const { body } = req;
+  const { name, address, data_type, sampling_interval } = body;
 
-    const validIntervals = [500, 1000, 5000];
+  const validIntervals = [500, 1000, 5000];
 
-    if (typeof name !== 'string' || 
-        typeof address !== 'string' ||
-        typeof data_type !== 'string' ||
-        typeof sampling_interval !== 'number'||
-        !validIntervals.includes(sampling_interval)) {
+  if (
+    typeof name !== "string" ||
+    typeof address !== "string" ||
+    typeof data_type !== "string" ||
+    typeof sampling_interval !== "number" ||
+    !validIntervals.includes(sampling_interval)
+  ) {
+    return res.status(400).json({ message: "Invalid variable data" });
+  }
 
-        return res.status(400).json({message: 'Invalid variable data'});
-    }
-
-    try {
-        const dbRes = await pool.query(`
+  try {
+    const dbRes = await pool.query(
+      `
         INSERT INTO config (
             name,
             address,
@@ -44,104 +44,100 @@ router.post('/', async (req, res) => {
             $3,
             $4
         ) RETURNING *;
-        `, 
-        [name, address, data_type, sampling_interval]
-        );
+        `,
+      [name, address, data_type, sampling_interval],
+    );
 
-        res.status(201).json(dbRes.rows[0]);
-
-    } catch (e) {
-        console.error('Errore POST:', e)
-        res.status(500)
-            .json({
-                message: 'Error posting new variables'
-            });
-    };
+    res.status(201).json(dbRes.rows[0]);
+  } catch (e) {
+    console.error("Errore POST:", e);
+    res.status(500).json({
+      message: "Error posting new variables",
+    });
+  }
 });
 
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
 
-    try {
-        const dbRes = await pool.query(`
+  try {
+    const dbRes = await pool.query(
+      `
         DELETE FROM config WHERE id = $1 RETURNING *    
-        `, 
-        [id]
-        );
-        
-        if (dbRes.rows.length === 0) {
-            return res.status(404)
-                .json({
-                    message: 'Variable not found'
-                }); 
-        }
+        `,
+      [id],
+    );
 
-        res.status(200).json(dbRes.rows[0]);
+    if (dbRes.rows.length === 0) {
+      return res.status(404).json({
+        message: "Variable not found",
+      });
+    }
 
-    } catch (e) {
-        res.status(500)
-            .json({
-                message: 'Error deleting variable'
-            });
-    };
-
+    res.status(200).json(dbRes.rows[0]);
+  } catch (e) {
+    res.status(500).json({
+      message: "Error deleting variable",
+    });
+  }
 });
 
-router.post('/apply', async (req, res) => {
-    
-    const { body } = req;
-    const { variables } = body;
+router.post("/apply", async (req, res) => {
+  const { body } = req;
+  const { variables } = body;
 
-    if (!Array.isArray(variables)) {
-        return res.status(400);
-    }
+  if (!Array.isArray(variables)) {
+    return res.status(400);
+  }
 
-    if (variables.length === 0) {
-        return res.status(400);
-    }
-    
-    const client = await pool.connect();
-    
-    try {
-        await client.query('BEGIN');
-        await client.query('TRUNCATE config RESTART IDENTITY CASCADE');
+  if (variables.length === 0) {
+    return res.status(400);
+  }
 
-        for (const variable of variables) {
-            const { name, address, data_type, sampling_interval } = variable;
+  const client = await pool.connect(); // Dovrebbe stare in un try catch probabilmente
 
-            const validIntervals = [500, 1000, 5000];
+  try {
+    await client.query("BEGIN");
+    await client.query("TRUNCATE config RESTART IDENTITY CASCADE");
 
-            if (typeof name !== 'string' || 
-                typeof address !== 'string' ||
-                typeof data_type !== 'string' ||
-                typeof sampling_interval !== 'number'||
-                !validIntervals.includes(sampling_interval)) {
-                
-                await client.query('ROLLBACK');
-                return res.status(400).json({message: 'Invalid variable data'});
-            }
+    for (const variable of variables) {
+      const { name, address, data_type, sampling_interval } = variable;
 
-            await client.query(`
+      const validIntervals = [500, 1000, 5000];
+
+      if (
+        typeof name !== "string" ||
+        typeof address !== "string" ||
+        typeof data_type !== "string" ||
+        typeof sampling_interval !== "number" ||
+        !validIntervals.includes(sampling_interval)
+      ) {
+        await client.query("ROLLBACK");
+        return res.status(400).json({ message: "Invalid variable data" });
+      }
+
+      await client.query(
+        `
                 INSERT INTO config (
                 name, address, data_type, sampling_interval
                 ) VALUES (
                 $1, $2, $3, $4
                 )
-            `, 
-            [name, address, data_type, sampling_interval])
-        }
-        await client.query('COMMIT');
-        res.status(201).json({message: 'Configuration applied successfully'});
-
-    } catch (e) {
-        await client.query('ROLLBACK');
-        res.status(500)
-            .json({
-                message: "Error applying configuration, fallback"
-            });
-    } finally {
-        client.release();
+            `,
+        [name, address, data_type, sampling_interval],
+      );
     }
+    await client.query("COMMIT");
+    res.status(201).json({ message: "Configuration applied successfully" });
+  } catch (e) {
+    await client.query("ROLLBACK");
+    res.status(500).json({
+      message: "Error applying configuration, fallback",
+    });
+  } finally {
+    client.release();
+  }
 });
 
-module.exports = router
+module.exports = router;
+
--- backend/services/s7/s7Service.js
+++ backend/services/s7/s7Service.js
@@ -1,46 +1,78 @@
-require('dotenv').config();
+const nodes7 = require("nodes7");
 
-const nodes7 = require('nodes7');
+function connectPromise(conn, ip, port, rack, slot) {
+  return new Promise((resolve, reject) => {
+    conn.initiateConnection(
+      {
+        host: ip,
+        port: port,
+        rack: rack,
+        slot: slot,
+      },
+      (e) => {
+        if (e) {
+          reject(e);
+        } else {
+          resolve();
+        }
+      },
+    );
+  });
+}
+
+function dropPromise(conn) {
+  return new Promise((resolve, reject) => {
+    conn.dropConnection((e) => {
+      if (e) reject(e);
+      else resolve();
+    });
+  });
+}
 
 class S7Service {
-    constructor () {
-        this.conn = new nodes7();
-        this.connected = false;
+  constructor() {
+    this.conn = new nodes7();
+    this.connected = false;
+  }
+
+  async connect(ip, port, rack, slot, addresses) {
+    try {
+      await connectPromise(this.conn, ip, port, rack, slot);
+      this.connected = true;
+      this.conn.addItems(addresses);
+    } catch (e) {
+      this.connected = false;
+      throw new Error(`Connection failed: ${e.message}`);
     }
+  }
 
-    async connect (ip, port, rack, slot) {
-        try {
-            this.conn.initiateConnection({
-                host: ip,
-                port: port,
-                rack: rack,
-                slot: slot
-            }, (e) => e)
-
-            this.connected = true;
-        } catch (e) {
-            this.connected = false;
-        }
+  async reconnect(ip, port, rack, slot, addresses) {
+    try {
+      await this.disconnect();
+      await this.connect(ip, port, rack, slot, addresses);
+    } catch (e) {
+      throw new Error(`Error while reconnecting: ${e.message}`);
     }
+  }
 
-    async reconnect (ip, port, rack, slot) {
-        this.conn.dropConnection()
-        
+  async disconnect() {
+    if (this.conn && this.connected) {
+      try {
+        await dropPromise(this.conn);
+      } catch (e) {
+        throw new Error(`Error while disconnecting: ${e.message}`);
+      }
     }
+  }
 
-    async disconnect() {
-        if (this.conn && this.connected) {
-            this.conn.dropConnection()
-                .then(
-                    this.connected = false;
-                );
-        }
+  async readVariables(variables) {
+    try {
+      const res = await this.conn.readVariables();
+      return res;
+    } catch (e) {
+      throw new Error(`Error reading variables: ${e.message}`);
     }
-
-    async readVariables(variables) {
-
-    }
-
+  }
 }
 
 module.exports = S7Service;
